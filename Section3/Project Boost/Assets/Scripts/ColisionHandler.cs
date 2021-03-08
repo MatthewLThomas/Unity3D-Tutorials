e6d6ff88f1d212b45c808f28fd505e3427b557f5
Matthew Thomas <71943868+MatthewLThomas@users.noreply.github.com>
2021-03-07 18:45:27 -0600

    init commit

--- Section3/Project Boost/Assets/Scripts/ColisionHandler.cs
+++ Section3/Project Boost/Assets/Scripts/ColisionHandler.cs
@@ -5,6 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class ColisionHandler : MonoBehaviour
 {
+    [SerializeField] AudioSource audio1;
+    [SerializeField] AudioSource audio2;
+
+    [SerializeField] AudioSource audio3;
+
+    [SerializeField] AudioClip success;
+    [SerializeField] ParticleSystem successEffect;
+    [SerializeField] AudioClip crash;
+    [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] float delay = 1;
+
+    bool isTransitioning = false;
     private void OnCollisionEnter(Collision other) {
         switch (other.gameObject.tag)
         {
@@ -12,17 +24,41 @@ public class ColisionHandler : MonoBehaviour
             case "Friendly": Debug.Log("Hey, all! You're Free to launch"); break;
             case "Fuel": Debug.Log("You got fuel"); break;
             case "Finish": Debug.Log("You delivered your cargo!"); 
-                LoadNextScene();
+                StartNewSceneSequened();
                 break;
             default: Debug.Log("You crashed"); 
-                ReloadScene();
+                StartCrashSequence();
             break;
         }
     }
 
+    //TODO: Fix Crash / Land Sound Bug
+    private void StartCrashSequence(){
+        GetComponent<Movemenet>().enabled = false;
+        audio1.enabled = false;
+        audio2.enabled = false;
+        if(!isTransitioning){
+            audio3.PlayOneShot(crash);
+            crashEffect.Play();
+            isTransitioning = !isTransitioning;
+        }
+        Invoke("ReloadScene", delay/2);
+    }        
     private void ReloadScene(){
         //reloads active scene
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void StartNewSceneSequened(){
+        GetComponent<Movemenet>().enabled = false;
+        audio1.enabled = false;
+        audio2.enabled = false;
+        if(!isTransitioning){
+            audio3.PlayOneShot(success);
+            successEffect.Play();
+            isTransitioning = !isTransitioning;
+        }
+        Invoke("LoadNextScene", delay);
     }
     private void LoadNextScene(){
         //Load Next Scene
--- Section3/Project Boost/Assets/Scripts/Movemenet.cs
+++ Section3/Project Boost/Assets/Scripts/Movemenet.cs
@@ -4,36 +4,87 @@ using UnityEngine;
 
 public class Movemenet : MonoBehaviour
 {
-    Rigidbody rb;
+    
     [SerializeField] float thrust = 2;
     [SerializeField] float wheelForce = 2;
+    [SerializeField] ParticleSystem frontRightThrust;
+    [SerializeField] ParticleSystem frontLeftThrust;
+    [SerializeField] ParticleSystem backRightThrust;
+
+    [SerializeField] ParticleSystem BackLeftThrust;
 
     [SerializeField] AudioSource enigeneThrust;
     [SerializeField] AudioSource rcsThrust;
+
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        FrontThrustEnable();
+        BackThrustEnable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessRotation(Input.GetAxis("Horizontal"));
-        ProcessThrust(Input.GetAxis("Jump"));
+        float wheel = ProcessRotation(Input.GetAxis("Horizontal"));
+        float throttle = ProcessThrust(Input.GetAxis("Jump"));
+        FrontThrustSpeed(throttle, wheel);
+        BackThrustSpeed(throttle, wheel);
     }
-    void ProcessThrust(float throttle){
+
+    void BackThrustSpeed(float throttle, float wheel){
+        
+        if (wheel < 0) {
+            backRightThrust.startSpeed = 10*(throttle);
+            BackLeftThrust.startSpeed = 10*(throttle);
+        }else{
+            backRightThrust.startSpeed = 10*(throttle+wheel);
+            BackLeftThrust.startSpeed = 10*(throttle+wheel);
+        }
+    }
+    void FrontThrustSpeed(float throttle, float wheel){
+        if (wheel > 0) {
+            frontRightThrust.startSpeed = 10*(throttle);
+            frontLeftThrust.startSpeed = 10*(throttle);
+        }else{
+            frontRightThrust.startSpeed = 10*(throttle-wheel);
+            frontLeftThrust.startSpeed = 10*(throttle-wheel);
+        }
+    }
+
+    public void FrontThrustEnable(){
+        frontLeftThrust.Play();
+        frontRightThrust.Play();
+    }
+    public void BackThrustEnable(){
+        backRightThrust.Play();
+        BackLeftThrust.Play();
+    }
+    public void FrontThrustDisable(){
+        frontLeftThrust.Stop();
+        frontRightThrust.Stop();
+    }
+    public void BackThrustDisable(){
+        backRightThrust.Stop();
+        BackLeftThrust.Stop();
+    }
+    float ProcessThrust(float throttle){
         rb.AddRelativeForce(Vector3.right * throttle * thrust*Time.deltaTime);
         if(throttle > 0f){
             if(!enigeneThrust.isPlaying){  
                 enigeneThrust.Play();
+                
             }
         }else{
             enigeneThrust.Stop();
+           
         }
         enigeneThrust.volume = throttle;
+        return throttle;
     }
-    void ProcessRotation(float wheel){ 
+    float ProcessRotation(float wheel){ 
         rb.AddRelativeTorque(Vector3.back*wheel*wheelForce*Time.deltaTime);
         if(wheel != 0){
             if(!rcsThrust.isPlaying){
@@ -43,6 +94,7 @@ public class Movemenet : MonoBehaviour
             rcsThrust.Stop();
         }
         rcsThrust.volume = Mathf.Abs(wheel)/2;
+        return wheel;
     }
     
        
